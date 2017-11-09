c7e8c725cf08bd4ac10b8c1b46f107dc26cfbf51
parkertdrake <parkertdrake@gmail.com>
2017-11-08 22:10:02 -0600

    changes to kestrel class and scratchwork

--- PlayerShips.py
+++ PlayerShips.py
@@ -1,18 +1,19 @@
 """
-A class to hold the Kestrel Ship. Definitely this will need to be extended in the long term for multiple player ships, but we'll start here.
-
-
+A module to hold player ships.
 """
 
 import ShipComponents
 
+"""
+Class for the Kestrel, default player ship
+"""
 class Kestrel:
     def __init__(self):
         self.hull = 30
         self.reactor_available = 0
-        self.shields = ShipComponents.ShieldSystem()
-        self.weapons = ShipComponents.WeaponSystem()
-        self.engines = ShipComponents.EngineSystem()
-        self.medbay = ShipComponents.MedBaySystem()
-        self.crew = ShipComponents.Crew()
+        self.shields = ShipComponents.ShieldSystem(2, 2, 2) # default Kestrel is fully powered 1 bubble shield
+        #self.weapons = ShipComponents.WeaponSystem()
+        #self.engines = ShipComponents.EngineSystem()
+        #self.medbay = ShipComponents.MedBaySystem()
+        #self.crew = ShipComponents.Crew()
 
--- Scratchwork.py
+++ Scratchwork.py
@@ -1,48 +1,9 @@
-import numpy as np
-from PIL import ImageGrab
-import cv2
-from win32gui import GetForegroundWindow, GetWindowRect
+import pytesseract
+import Utility
+from PIL import Image
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
+#print pytesseract.image_to_string(Image.open('test.jpg'))
 
-import time
-
-
-# this function records a picture of the active window.
-# using this I can count on my images of the game always being lined up.
-
-
-def screen_record():
-
-    i = 0
-    while(True):
-        # 800x600 windowed mode
-
-        handle = GetForegroundWindow()
-        rect = GetWindowRect(handle)
-        x = rect[0]
-        y = rect[1]
-        farx = rect[2]
-        fary = rect[3]
-        print x, y, farx, fary
-
-        time.sleep(1)
-        # starts at 0, 0 (upper left hand corner) and grabs an 800 by 640 image.
-        printscreen = np.array(ImageGrab.grab(bbox=(x, y, farx, fary)))
-        #cv2.imshow('window', cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
-        cv2.imwrite('screen_grab' + str(i) + '.png', cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
-
-        i += 1
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
-
-
-# gives us time to get situated in the game
-for i in list(range(3))[::-1]:
-    print(i+1)
-    time.sleep(1)
-
-while True:
-    screen_record()
-
-
-
+image = Utility.screen_grab()
+text = Utility.image_text(image)
+print text
