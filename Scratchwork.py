dae61a85b61d896f02018fdd52bb2c81813f3905
parkertdrake <parkertdrake@gmail.com>
2017-11-22 19:55:15 -0600

    informal power testing

--- Scratchwork.py
+++ Scratchwork.py
@@ -1,18 +1,18 @@
 import pytesseract
 from GameLibrary import Encounter
 import Utility
+import pyautogui
 from time import sleep
 from PIL import Image
+from Commands import PowerCommand
 import cv2
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
 #print pytesseract.image_to_string(Image.open('test.jpg'))
 
 
 
-# Utility.load_image_from_file("Tests/TestInputs")
-
-# Demo code
-enc = Encounter()
 Utility.countdown(5)
+enc = Encounter()
 enc.update()
-enc.print_player_status()
+power_command = PowerCommand(enc.player_ship.engines, 0)
+power_command.execute()
