b43237d63db1e0fa62ddddb784177ad4dfaa69c8
parkertdrake <parkertdrake@gmail.com>
2017-11-24 16:41:36 -0600

    minor edits

--- Scratchwork.py
+++ Scratchwork.py
@@ -7,10 +7,6 @@ from PIL import Image
 from Commands import PowerCommand
 import cv2
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
-#print pytesseract.image_to_string(Image.open('test.jpg'))
 
 
 
-for i in range(22):
-    print "self.matrix[x][y] = self.doors[" + str(i) + "]"
-    print "self.matrix[y][x] = self.doors[" + str(i) + "]"
