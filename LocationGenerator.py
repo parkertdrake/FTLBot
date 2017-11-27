4d70203f124f7dcc09ddf4473427ba6bb0fdf5f1
parkertdrake <parkertdrake@gmail.com>
2017-11-27 14:06:09 -0600

    system segment generator

--- LocationGenerator.py
+++ LocationGenerator.py
@@ -1,6 +1,6 @@
 """
 File to generate location code for some of the locations module
-Just so I don't have to retype some of this if it changes
+Just so I don't have to retype all of them if it changes
 """
 
 """
@@ -48,9 +48,44 @@ def generate_enemy_hull_segments(row, starting_col, col_width):
 
 """
 Generate segment locations for all major systems. Shields, engines, medbay, O2, and weapons
-
+@:param starting row of lowest segment
+@:param starting col of leftmost system
+@:param row_height distance between rows
+@:param col_width distance between columns
+@:param seg_height height of segments
+@:param seg_width width of segments
 """
-def generate_system_segments(
+def generate_system_segments(starting_row, starting_col, row_height, col_width, seg_height, seg_width):
+    num_shield_segments = 2
+    num_engine_segments = 2
+    num_medbay_segments = 1
+    num_oxygen_segments = 1
+    num_weapon_segments = 2
+    segments = [num_shield_segments, num_engine_segments, num_medbay_segments, num_oxygen_segments, num_weapon_segments]
+    titles = ["SHIELD_SEGMENTS", "ENGINE_SEGMENTS", "MEDBAY_SEGMENTS", "OXYGEN_SEGMENTS", "WEAPON_SEGMENTS"]
+    col = starting_col
+    for i in range (len(titles)):
+        system = titles[i]
+        print system + " = ["
+        row = starting_row
+        num_segments = segments[i]
+        print "[",
+        for j in range (num_segments):
+            #generate 10 pixel locations across the segment
+            pix_width = seg_width / 10
+            pix_row = row
+            pix_col = col
+            for k in range(10):
+                print "(" + str(pix_row) + ", " + str(pix_col) + ")",
+                if k < 9:
+                    print ","
+                pix_col += pix_width
+            row -= row_height
+            print "], "
+        print "]"
+        col += col_width
 
 
-generate_player_hull_segments(128, 33, 24)
+
+
+generate_system_segments(1430, 165, 16, 73, 100, 30)
---
+++ Locations.py
@@ -0,0 +1,58 @@
+"""
+All hardcoded pixel locations are going to live here.
+In the event a pixel location needs to change, it changes here.
+"""
+
+# reactor power
+REACTOR_SEGMENTS = [ (1484, 45) , (1464, 45) , (1444, 45) , (1424, 45) , (1404, 45) , (1384, 45) , (1364, 45) , (1344, 45) ]
+
+# hull health
+PLAYER_HULL_SEGMENTS = [ (128, 33)
+, (128, 57)
+, (128, 81)
+, (128, 105)
+, (128, 129)
+, (128, 153)
+, (128, 177)
+, (128, 201)
+, (128, 225)
+, (128, 249)
+, (128, 273)
+, (128, 297)
+, (128, 321)
+, (128, 345)
+, (128, 369)
+, (128, 393)
+, (128, 417)
+, (128, 441)
+, (128, 465)
+, (128, 489)
+, (128, 513)
+, (128, 537)
+, (128, 561)
+, (128, 585)
+, (128, 609)
+, (128, 633)
+, (128, 657)
+, (128, 681)
+, (128, 705)
+, (128, 729)
+]
+ENEMY_HULL_SEGMENTS = None
+
+# power/health of all major systems
+SHIELD_SEGMENTS =
+ENGINE_SEGMENTS =
+MEDBAY_SEGMENTS =
+OXYGEN_SEGMENTS =
+WEAPON_SEGMENTS =
+
+# shield bubbles
+PLAYER_BUBBLES =
+ENEMY_BUBBLES =
+
+# rooms on the Kestrel
+ROOM_LOCATIONS =
+
+# doors on the Kestrel
+DOOR_LOCATIONS =
