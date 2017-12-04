a1b4a49bca83c6af1cb9fbe536152cc15f39ffad
parkertdrake <parkertdrake@gmail.com>
2017-12-04 15:17:41 -0600

    uncommented weapon system constructor

--- PlayerShips.py
+++ PlayerShips.py
@@ -11,13 +11,12 @@ class Kestrel:
     def __init__(self):
         self.hull = 30
         self.reactor_available = 0
+
         self.shields = ShipComponents.ShieldSystem(2, 2, 2) # default Kestrel is fully powered 1 bubble shield
         self.engines = ShipComponents.EngineSystem(2, 2, 2)
         self.medbay = ShipComponents.MedBaySystem(1, 1, 1) # 1 power medbay
         self.oxygen = ShipComponents.OxygenSystem(1, 1, 1) # default oxygen system
-
-        #self.weapons = ShipComponents.WeaponSystem()
-        self.weapons = None
+        self.weapons = ShipComponents.WeaponSystem(3, 3, 0) # default weapon system
 
         self.rooms = ShipComponents.RoomSystem(self.shields,
                                                self.engines,
