8fc2b17ff5e4be6438e6fa78092a75c7bfdbcced
parkertdrake <parkertdrake@gmail.com>
2017-12-04 19:15:34 -0600

    informal vent testing

--- Scratchwork.py
+++ Scratchwork.py
@@ -5,15 +5,17 @@ import Locations
 import pyautogui
 from time import sleep
 from PIL import Image
-from Commands import PowerCommand
+from Commands import PowerCommand, VentCommand
 import cv2
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
 
 
 Utility.countdown(3)
 enc = Encounter()
-enc.update()
-print enc.player_ship.weapons.artemis.powered
-print enc.player_ship.weapons.burst_laser.powered
+rooms = enc.player_ship.rooms
+engine_room = rooms.rooms[3]
+vent_command = VentCommand(engine_room,rooms)
+vent_command.execute()
+
 
 
