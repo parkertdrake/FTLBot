f0ccc821d9bde7bfa7456e411c2e06effe5b2786
parkertdrake <parkertdrake@gmail.com>
2017-12-11 15:54:03 -0600

    refactoring parameters

--- Tests/LibraryTests.py
+++ Tests/LibraryTests.py
@@ -9,19 +9,19 @@ Unit tests for utility functions (the ones that make sense anyway)
 class TestLibrary(unittest.TestCase):
     def test_player_hull(self):
         img = Utility.load_image_from_file("reference.png")
-        enc = GameLibrary.Encounter()
+        enc = GameLibrary.Encounter(pause=False)
         enc.update_player_health(img)
         assert enc.player_ship.hull == 27
 
     def test_player_reactor(self):
         img = Utility.load_image_from_file("reference.png")
-        enc = GameLibrary.Encounter()
+        enc = GameLibrary.Encounter(image=img, pause=False)
         enc.update_reactor(img)
         assert enc.player_ship.reactor_available == 4
 
     def test_player_shield(self):
         img = Utility.load_image_from_file("reference.png")
-        enc = GameLibrary.Encounter()
+        enc = GameLibrary.Encounter(image=img, pause=False)
         enc.update_player_shield(img)
         # need to check health, power_level, and bubbles
         assert enc.player_ship.shields.health == 2
@@ -30,71 +30,78 @@ class TestLibrary(unittest.TestCase):
 
     def test_player_engine(self):
         img = Utility.load_image_from_file("reference.png")
-        enc = GameLibrary.Encounter()
+        enc = GameLibrary.Encounter(image=img, pause=False)
         enc.update_player_engine(img)
         assert enc.player_ship.engines.power_level == 1
         assert enc.player_ship.engines.health == 2
 
     def test_player_medbay(self):
         img = Utility.load_image_from_file("reference.png")
-        enc = GameLibrary.Encounter()
+        enc = GameLibrary.Encounter(image=img, pause=False)
         enc.update_player_medbay(img)
         assert enc.player_ship.medbay.power_level == 0
         assert enc.player_ship.medbay.health == 0
 
     def test_player_oxygen(self):
         img = Utility.load_image_from_file("reference.png")
-        enc = GameLibrary.Encounter()
+        enc = GameLibrary.Encounter(image=img, pause=False)
         enc.update_player_oxygen(img)
         assert enc.player_ship.oxygen.power_level == 1
         assert enc.player_ship.oxygen.health == 1
 
     def test_player_weapons(self):
         img = Utility.load_image_from_file("reference.png")
-        enc = GameLibrary.Encounter()
+        enc = GameLibrary.Encounter(image=img, pause=False)
         enc.update_player_weapons(img)
         assert enc.player_ship.weapons.artemis.ready == False
         assert enc.player_ship.weapons.burst_laser.ready == False
 
     def test_player_doors(self):
         img = Utility.load_image_from_file("reference.png")
-        enc = GameLibrary.Encounter()
+        enc = GameLibrary.Encounter(image=img, pause=False)
         enc.update_player_doors(img)
         assert enc.player_ship.rooms.doors[1].is_open == False
 
     # all together now
     def test_update(self):
         img = Utility.load_image_from_file("reference.png")
-        enc = GameLibrary.Encounter()
+        enc = GameLibrary.Encounter(image=img, pause=False)
         enc.update(img)
         assert enc.player_ship.hull == 27
 
     # testing enemy stuff
     def test_identify_enemy(self):
         img = Utility.load_image_from_file("reference.png")
-        enc = GameLibrary.Encounter()
+        enc = GameLibrary.Encounter(image=img, pause=False)
         enemy_string = enc.identify_enemy(img)
         assert (enemy_string == "Class: Rebel Fighter")
 
     def test_enemy_hull(self):
         img = Utility.load_image_from_file("eference.png")
-        enc = GameLibrary.Encounter()
+        enc = GameLibrary.Encounter(image=img, pause=False)
         enc.update_enemy_health(img)
         assert enc.enemy_ship.hull == 9
 
     def test_enemy_shield(self):
         img = Utility.load_image_from_file("reference.png")
-        enc = GameLibrary.Encounter()
+        enc = GameLibrary.Encounter(image=img, pause=False)
         enc.update_enemy_shield(img)
         assert enc.enemy_ship.bubbles == 1
 
+    #this doesn't actually test a library function, it tests a utility function
     def test_enemy_scan(self):
         img = Utility.load_image_from_file("reference.png")
-        weapons_loc = Utility.scan_for_image(img, Utility.load_image_from_file("weapons.png"))
-        shields_loc = Utility.scan_for_image(img, Utility.load_image_from_file("shields.png"))
-        engines_loc = Utility.scan_for_image(img, Utility.load_image_from_file("engines.png"))
-        oxygen_loc = Utility.scan_for_image(img, Utility.load_image_from_file("oxygen.png"))
-        helm_loc = Utility.scan_for_image(img, Utility.load_image_from_file("helm.png"))
+        enc = GameLibrary.Encounter(image=img, pause=False)
+        enc.update(img)
+        weapons_loc = enc.enemy_ship.weapons.location
+        shields_loc = enc.enemy_ship.shields.location
+        engines_loc = enc.enemy_ship.engines.location
+        oxygen_loc = enc.enemy_ship.oxygen.location
+        helm_loc = enc.enemy_ship.helm.location
+
+        print weapons_loc
+        print shields_loc
+
         assert weapons_loc[0] == 703
         assert weapons_loc[1] == 2057
 
@@ -110,9 +117,9 @@ class TestLibrary(unittest.TestCase):
         assert oxygen_loc[0] == 559
         assert oxygen_loc[1] == 2230
 
-
     def test_enemy_systems(self):
         img = Utility.load_image_from_file("reference.png")
-        enc = GameLibrary.Encounter()
-        assert True
+        enc = GameLibrary.Encounter(pause=False)
+        enc.update(img)
+        assert enc.enemy_ship.weapons.health == 2
 
